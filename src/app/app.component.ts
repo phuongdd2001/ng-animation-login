6def71922a4b47de2dcafa036aca965300eafd23
phuong1772001 <phuongdd@volio.vn>
2024-04-16 16:52:42 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 
+import '../assets/login-animation.js';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ng-animation-login';
+  validateForm!: UntypedFormGroup;
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log(`email: ${this.email} password: ${this.password}`);
+    alert(`Email: ${this.email} Password: ${this.password}`);
+  }
+
+  ngAfterViewInit(): void {
+    (window as any).initialize();
+  }
 }
